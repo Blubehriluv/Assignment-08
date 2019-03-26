94a4307d853171d338a5556aa35048ed13220754
Taylor McKenzie <44045526+Blubehriluv@users.noreply.github.com>
2019-03-26 13:23:05 -0700

    Added Playername and Color Change

--- Assignment 08/Interactive Fiction/Source.cpp
+++ Assignment 08/Interactive Fiction/Source.cpp
@@ -5,5 +5,16 @@ using namespace std;
 
 int main()
 {
+	cout << "Welcome to the 'Land of Life' Text Adventure!" << endl;
+	system("Pause");
+
+	cout << "What is the name of your character?" << endl;
+	string playerName;
+	cin >> playerName;
+
+	//For color
+	system("Color 1A");
+	cout << "";
+
 	system("pause");
 }
